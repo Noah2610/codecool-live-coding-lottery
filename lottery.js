5770a935f67e42c760596475fd196ddf456ddc07
Noah Rosenzweig <noah.rosenzweig@codecool.com>
2023-01-10 09:27:13 +0100

    Cleanup comments, rename constants, prep for next live coding

--- lottery.js
+++ lottery.js
@@ -1,16 +1,13 @@
 const prompt = require("prompt-sync")();
 
-// Generate random lottery numbers
-// picked from number set 1 - 20
-
-const LOTTERY_NUMBERS_LENGTH = 6;
-const RANDOM_NUMBER_RANGE = 20;
+const LOTTERY_TICKET_LENGTH = 6;
+const MAX_LOTTERY_NUMBER = 20;
 
 function generateLotteryNumbers() {
     const lotteryNumbers = [];
 
-    while (lotteryNumbers.length < LOTTERY_NUMBERS_LENGTH) {
-        const number = Math.floor(Math.random() * RANDOM_NUMBER_RANGE) + 1;
+    while (lotteryNumbers.length < LOTTERY_TICKET_LENGTH) {
+        const number = Math.floor(Math.random() * MAX_LOTTERY_NUMBER) + 1;
 
         if (!lotteryNumbers.includes(number)) {
             lotteryNumbers.push(number);
@@ -21,49 +18,36 @@ function generateLotteryNumbers() {
 }
 
 function getUserTicket() {
-    // User input:
-    // for picking their numbers
-    //
-    // Style of input:
-    // input 6 numbers separated by comma
-    //   5, 1, 2, 6, 2, 3
-    //
-    // Steps:
-    // - prompt user to input comma-separated numbers
-    //   - tell user that numbers are between 1 and 20
     console.log(
-        `Please input ${LOTTERY_NUMBERS_LENGTH} numbers between` +
-        ` 1 and ${RANDOM_NUMBER_RANGE}, separated by comma` +
+        `Please input ${LOTTERY_TICKET_LENGTH} numbers between` +
+        ` 1 and ${MAX_LOTTERY_NUMBER}, separated by comma` +
         "\nEvery number should only appear once"
     );
 
-    // - get user input string
     const userInput = prompt("Input: ");
 
-    // - split by comma separator
-    //   "1, 2, 3" => ["1", "2", "3"]
-    //   "1,2,3"   => ["1", "2", "3"]
-    const userInputSplit = userInput.split(",");
+    const userInputCommaSeparated = userInput.split(",");
     const lotteryTicket = [];
 
-    for (let i = 0; i < userInputSplit.length; i++) {
-        const item = userInputSplit[i];
-        lotteryTicket.push(parseInt(item));
+    for (let i = 0; i < userInputCommaSeparated.length; i++) {
+        const number = parseInt(userInputCommaSeparated[i]);
+
+        // TODO: Validate input
+        //   - check if they are valid numbers
+        //   - check if numbers are between 1 and 20
+        //   - check for duplicate numbers
+
+        lotteryTicket.push(number);
     }
 
     return lotteryTicket;
-
-    // - validate input
-    //   - check if they are valid numbers
-    //   - check if numbers are between 1 and 20
-    //   - check for duplicate numbers
 }
 
 const lotteryNumbers = generateLotteryNumbers();
-console.log(lotteryNumbers);
+console.log(`Secret lottery numbers: ${lotteryNumbers}`);
 
-const lotteryTicket = getUserTicket();
-console.log(lotteryTicket);
+const userLotteryTicket = getUserTicket();
+console.log(`User's lottery ticket: ${userLotteryTicket}`);
 
 // Check for winning ticket
 // joker numbers
